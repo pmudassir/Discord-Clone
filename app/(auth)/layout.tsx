162d13ff8e09609e5fe03460ba315c21d25ee33b
Mudassir Muhammed <96933276+pmudassir@users.noreply.github.com>
2024-01-02 14:10:37 +0530

    authenticatoin

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full items-center justify-center flex">{children}</div>;
+};
+
+export default AuthLayout;
